fd2b71c34bc166ae527723a3f5c43bda3f8cf93e
bulenty584 <bulenty@ucla.edu>
2025-11-14 10:24:34 -0800

    project card fixed

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,29 +1,4 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
-import './ProjectCard.css';
-import BruinRide from '../../Assets/BruinRide.png';
-import PnetPhlix from '../../Assets/PnetPhlix.png';
-import PeachParty from '../../Assets/PeachParty.png';
-import shufpy from '../../Assets/shufpy.gif';
-import { FaGithub } from "react-icons/fa";
-
-function ProjectCard(props) {
-    const [clicked, setClicked] = useState(false);
-
-    const handleClick = () => {
-        setClicked(!clicked);
-    }
-
-    const { description, title, githubLink } = props;
-
-    const imgs = {
-        "BruinRide": BruinRide,
-        "PnetPhlix": PnetPhlix,
-        "PeachParty": PeachParty,
-        "Shufpy": shufpy
-    };
-
-=======
 import "./ProjectCard.css";
 import BruinRide from "../../Assets/BruinRide.png";
 import PnetPhlix from "../../Assets/PnetPhlix.png";
@@ -47,7 +22,6 @@ function ProjectCard({ description, title, githubLink }) {
 
     const projectImage = IMAGE_MAP[title];
 
->>>>>>> 8137899 (new migration to newer packages and more updated cra build)
     return (
         <>
             {clicked && <div className="backdrop" onClick={handleClick}></div>}
@@ -71,12 +45,6 @@ function ProjectCard({ description, title, githubLink }) {
                         id="card" 
                         className="card-content p-4 bg-black rounded-lg border-4 border-black shadow-[8px_8px_0px_grey]"
                     >
-<<<<<<< HEAD
-                        <img 
-                            src={imgs[title]} 
-                            alt={title} 
-                            className={`h-48 w-full object-cover lg:rounded ${clicked ? 'fade-in' : ''}`} 
-=======
                         <img
                             src={projectImage}
                             alt={title}
@@ -86,7 +54,6 @@ function ProjectCard({ description, title, githubLink }) {
                             height={192}
                             sizes="(min-width: 1024px) 320px, (min-width: 640px) 45vw, 80vw"
                             className={`h-48 w-full object-cover lg:rounded ${clicked ? "fade-in" : ""}`}
->>>>>>> 8137899 (new migration to newer packages and more updated cra build)
                         />
                     </div>
                 )}
